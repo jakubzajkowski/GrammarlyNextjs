53cba44d97829769197d0a05981431642ee0ff58
Jakub Zajkowski <jakudjan123@gmail.com>
2023-08-10 16:29:13 +0200

    feat: fix props

--- src/app/account/docs/[id]/[user_id]/page.tsx
+++ src/app/account/docs/[id]/[user_id]/page.tsx
@@ -44,7 +44,7 @@ const Doc: React.FC<DocsProps>= ({params}) => {
 
   if (xd) return (
     <div className={styles.doc}>
-      <AnimatePresence>{isSidebar && (<SidebarDocument setIsSidebar={setIsSidebar} textRef={textRef} title={title} />)}</AnimatePresence>
+      <AnimatePresence>{isSidebar && (<SidebarDocument setIsSidebar={setIsSidebar} textRef={textRef} title={title as string} />)}</AnimatePresence>
       <div className={styles.doc__sidebar__nav}>
         <Link href='/account'><svg className={styles.nav__logo} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"/></svg></Link>
         <svg onClick={()=>setIsSidebar(true)} className={styles.nav__bars} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
